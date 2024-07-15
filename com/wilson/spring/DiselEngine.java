5342c32cb9a4da1ac5aae35b97931930c56376f9
Wilsonrangari <rangariwilson@gmail.com>
2024-07-15 11:10:30 +0200

    spring

---
+++ com/wilson/spring/DiselEngine.java
@@ -0,0 +1,13 @@
+package com.wilson.spring;
+
+public class DiselEngine implements IEngine {
+
+	@Override
+	public int start() {
+		System.out.println("Disel Engine Started");
+		return 1;
+		
+		
+	}
+
+}
---
+++ com/wilson/spring/IEngine.java
@@ -0,0 +1,7 @@
+package com.wilson.spring;
+
+public interface IEngine {
+	
+	public int start();
+
+}
